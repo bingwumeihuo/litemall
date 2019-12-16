78794fe9b77bf1b9c2721827776738fef74f7eeb
username <jmggwu@2472616135qq.com>
2019-12-17 00:52:33 +0800

    主页优化

--- litemall-admin/src/views/layout/components/Navbar.vue
+++ litemall-admin/src/views/layout/components/Navbar.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="navbar">
-    <hamburger :toggle-click="toggleSideBar" :is-active="sidebar.opened" class="hamburger-container"/>
+    <hamburger :toggle-click="toggleSideBar" :is-active="sidebar.opened" class="hamburger-container" />
 
-    <breadcrumb class="breadcrumb-container"/>
+    <breadcrumb class="breadcrumb-container" />
 
     <div class="right-menu">
       <template v-if="device!=='mobile'">
 
         <el-tooltip content="全屏" effect="dark" placement="bottom">
-          <screenfull class="screenfull right-menu-item"/>
+          <screenfull class="screenfull right-menu-item" />
         </el-tooltip>
 
         <el-tooltip content="布局大小" effect="dark" placement="bottom">
-          <size-select class="international right-menu-item"/>
+          <size-select class="international right-menu-item" />
         </el-tooltip>
 
       </template>
@@ -20,7 +20,7 @@
       <el-dropdown class="avatar-container right-menu-item" trigger="click">
         <div class="avatar-wrapper">
           <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
-          <i class="el-icon-caret-bottom"/>
+          <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown">
           <router-link to="/">
@@ -28,16 +28,6 @@
               首页
             </el-dropdown-item>
           </router-link>
-          <el-dropdown-item divided>
-            <a target="_blank" href="https://github.com/linlinjava/litemall">
-              GitHub
-            </a>
-          </el-dropdown-item>
-          <el-dropdown-item>
-            <a target="_blank" href="https://gitee.com/linlinjava/litemall">
-              码云
-            </a>
-          </el-dropdown-item>
           <el-dropdown-item divided>
             <router-link to="/profile/password">
               密码修改
